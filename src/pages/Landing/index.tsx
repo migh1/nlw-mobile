da8bad8c55176fddf0ad39efa474255930e9efcd
ojuara <lucas.araujo@ambar.tech>
2020-08-14 09:17:16 -0300

    first commit

---
+++ src/pages/Landing/index.tsx
@@ -0,0 +1,64 @@
+import { useNavigation } from '@react-navigation/native';
+import React, { useEffect, useState } from 'react';
+import { Image, Text, View } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+import giveClassesIcon from '../../assets/images/icons/give-classes.png';
+import heartIcon from '../../assets/images/icons/heart.png';
+import studyIcon from '../../assets/images/icons/study.png';
+import landingImg from '../../assets/images/landing.png';
+import api from '../../services/api';
+import styles from './styles';
+
+const Landing = () => {
+  const [totalConnections, setTotalConnections] = useState(0);
+  const { navigate } = useNavigation();
+
+  useEffect(() => {
+    api.get('connections').then((response) => {
+      const { total } = response.data;
+      setTotalConnections(total);
+    });
+  }, []);
+
+  const handleNavigateToGiveClassesPage = () => {
+    navigate('GiveClasses');
+  };
+
+  const handleNavigateStudyPages = () => {
+    navigate('Study');
+  };
+  return (
+    <View style={styles.container}>
+      <Image source={landingImg} style={styles.banner} />
+      <Text style={styles.title}>
+        Seja bem vindo, {'\n'}
+        <Text style={styles.titleBold}>O que deseja fazer?</Text>
+      </Text>
+
+      <View style={styles.buttonsContainer}>
+        <RectButton
+          onPress={handleNavigateStudyPages}
+          style={[styles.button, styles.buttonPrimary]}
+        >
+          <Image source={studyIcon} />
+          <Text style={styles.buttonText}>Estudar</Text>
+        </RectButton>
+
+        <RectButton
+          onPress={handleNavigateToGiveClassesPage}
+          style={[styles.button, styles.buttonSecondary]}
+        >
+          <Image source={giveClassesIcon} />
+          <Text style={styles.buttonText}>Dar aulas</Text>
+        </RectButton>
+      </View>
+
+      <Text style={styles.totalConnections}>
+        Total de {totalConnections} conexões já realizadas{' '}
+        <Image source={heartIcon} />
+      </Text>
+    </View>
+  );
+};
+
+export default Landing;
---
+++ src/pages/TeacherList/index.tsx
@@ -0,0 +1,160 @@
+import { Feather } from '@expo/vector-icons';
+import AsyncStorage from '@react-native-community/async-storage';
+import { useFocusEffect } from '@react-navigation/native';
+import React, { useState } from 'react';
+import { Text, View, Picker } from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import {
+  BorderlessButton,
+  RectButton,
+  ScrollView,
+  TextInput,
+} from 'react-native-gesture-handler';
+import PageHeader from '../../components/PageHeader';
+import TeacherItem, { Teacher } from '../../components/TeacherItem';
+import api from '../../services/api';
+import moment from 'moment';
+import styles from './styles';
+
+const TeacherList = () => {
+  const [isFiltersVisible, setIsFiltersVisible] = useState(false);
+  const [favorites, setFavorites] = useState<number[]>([]);
+  const [teachers, setTeachers] = useState([]);
+
+  const [subject, setSubject] = useState('Matemática');
+  const [weekDay, setWeekDay] = useState(1);
+  const [time, setTime] = useState(new Date());
+  const [show, setShow] = useState(false);
+
+  const loadFavorites = () => {
+    AsyncStorage.getItem('favorites').then((response) => {
+      if (response) {
+        const favoritedTeachers = JSON.parse(response);
+        const favoritedTeachersId = favoritedTeachers.map(
+          (teacher: Teacher) => teacher.id
+        );
+
+        setFavorites(favoritedTeachersId);
+      }
+    });
+  };
+
+  const handleToggleFiltersVisible = () => {
+    setIsFiltersVisible(!isFiltersVisible);
+  };
+
+  const handleFiltersSubmit = async () => {
+    loadFavorites();
+    const response = await api.get('classes', {
+      params: {
+        subject,
+        week_day: weekDay,
+        time: moment(time).format('HH:mm'),
+      },
+    });
+    setTeachers(response.data);
+    setIsFiltersVisible(false);
+  };
+
+  useFocusEffect(() => {
+    loadFavorites();
+  });
+
+  return (
+    <View style={styles.container}>
+      <PageHeader
+        title='Proffys disponíveis'
+        headerRight={
+          <BorderlessButton onPress={handleToggleFiltersVisible}>
+            <Feather name='filter' size={20} color='#fff' />
+          </BorderlessButton>
+        }
+      >
+        {isFiltersVisible && (
+          <View style={styles.searchForm}>
+            <Text style={styles.label}>Matéria</Text>
+            <TextInput
+              style={styles.input}
+              placeholder='Qual a máteria?'
+              placeholderTextColor='#c1bccc'
+              value={subject}
+              onChangeText={(text) => setSubject(text)}
+            />
+
+            <View style={styles.inputGroup}>
+              <View style={styles.inputBlock}>
+                <Text style={styles.label}>Dia da semana</Text>
+                <View style={styles.picker}>
+                  <Picker
+                    selectedValue={weekDay}
+                    onValueChange={(itemValue) => setWeekDay(itemValue)}
+                  >
+                    <Picker.Item label='Domingo' value={1} />
+                    <Picker.Item label='Segunda-Feira' value={2} />
+                    <Picker.Item label='Terça-Feira' value={3} />
+                    <Picker.Item label='Quarta-Feira' value={4} />
+                    <Picker.Item label='Quinta-Feira' value={5} />
+                    <Picker.Item label='Sexta-Feira' value={6} />
+                    <Picker.Item label='Sábado' value={7} />
+                  </Picker>
+                </View>
+              </View>
+
+              <View style={styles.inputBlock}>
+                <Text style={styles.label}>Horário</Text>
+                {show && (
+                  <DateTimePicker
+                    value={time}
+                    style={styles.picker}
+                    is24Hour
+                    onChange={(event, selectedDate) => {
+                      const currentDate = selectedDate || time;
+                      setShow(false);
+                      setTime(currentDate);
+                    }}
+                    mode='time'
+                    display='default'
+                  />
+                )}
+                <TextInput
+                  style={styles.input}
+                  value={moment(time).format('HH:mm')}
+                  onFocus={() => setShow(true)}
+                  placeholder='__:__'
+                />
+                {/* <TextInput
+                  style={styles.input}
+                  placeholder='Qual o horário?'
+                  placeholderTextColor='#c1bccc'
+                  value={time}
+                  onChangeText={(text) => setTime(text)}
+                /> */}
+              </View>
+            </View>
+
+            <RectButton
+              onPress={handleFiltersSubmit}
+              style={styles.submitButton}
+            >
+              <Text style={styles.submitButtonText}>Filtrar</Text>
+            </RectButton>
+          </View>
+        )}
+      </PageHeader>
+      <ScrollView
+        style={styles.teacherList}
+        contentContainerStyle={{ paddingHorizontal: 16, paddingBottom: 24 }}
+      >
+        {teachers.map((teacher: Teacher) => (
+          <TeacherItem
+            key={teacher.id}
+            teacher={teacher}
+            favorited={favorites.includes(teacher.id)}
+          />
+        ))}
+      </ScrollView>
+    </View>
+  );
+};
+
+export default TeacherList;
